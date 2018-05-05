6ae43f0f386dbded64684abec234d34985cf4e7f
Eric Ratliff <e@edr.xyz>
2018-05-04 23:16:28 -0500

    Containers now talk with one another and the website now contains a button that will cause the camera to take a picture when clicked

--- Camera-Controller/app.py
+++ Camera-Controller/app.py
@@ -1,6 +1,11 @@
 import logging
 import os
 import gphoto2 as gp
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
 def take_photo():
 
@@ -33,4 +38,10 @@ def take_photo():
 
     return photoFile
 
-take_photo()
+@app.route('/')
+def hello():
+    take_photo()
+    return '<b>Hello from the camera controller!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- Website/app.py
+++ Website/app.py
@@ -19,7 +19,15 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Woot hi!  I have been seen {} times.\n'.format(count)
+
+    html = """
+        <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+        <form action="http://0.0.0.0:5002" method="get" target="dummyframe">
+            <input type="submit" value="take pic"/>
+        </form>"
+        """
+
+    return html + '\nWoot hi!  I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
