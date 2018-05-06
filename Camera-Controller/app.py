caf596c71436483e6a4e41852e24c29bdf974e18
Eric Ratliff <e@edr.xyz>
2018-05-06 00:08:18 -0500

    Enabling more logging

--- Camera-Controller/app.py
+++ Camera-Controller/app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 
 def take_photo():
 
+    log = ""
+
     # Get the directory containing this script
     photoDir = os.path.dirname(os.path.realpath(__file__))
 
@@ -14,7 +16,7 @@ def take_photo():
     photoDirName = "photos"
     photoDir = os.path.join(photoDir, photoDirName)
     if not os.path.exists(photoDir):
-        print("Creating photo directory")
+        log += "Creating photo directory" + "\n"
         os.makedirs(photoDir)
     
     logging.basicConfig(
@@ -22,24 +24,24 @@ def take_photo():
     gp.check_result(gp.use_python_logging())
     camera = gp.check_result(gp.gp_camera_new())
     gp.check_result(gp.gp_camera_init(camera))
-    print('Capturing image')
+    log += 'Capturing image' + "\n"
     file_path = gp.check_result(gp.gp_camera_capture(
         camera, gp.GP_CAPTURE_IMAGE))
-    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    log += 'Camera file path: {0}/{1}'.format(file_path.folder, file_path.name) + "\n"
     photoFile = os.path.join(photoDir, file_path.name)
-    print('Copying image to', photoFile)
+    log += 'Copying image to', photoFile + "\n"
     camera_file = gp.check_result(gp.gp_camera_file_get(
             camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
     gp.check_result(gp.gp_file_save(camera_file, photoFile))
     #subprocess.call(['xdg-open', photoFile])
     gp.check_result(gp.gp_camera_exit(camera))
 
-    return photoFile, photoDir
+    return photoFile, photoDir, log
 
 @app.route('/')
 def hello():
-    photoFile, photoDir = take_photo()
-    return '<b>Hello from the camera controller!\n{}'.format(photoDir)
+    photoFile, photoDir, log = take_photo()
+    return '<b>Hello from the camera controller!\n{}\n\n{}'.format(photoDir, log)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
