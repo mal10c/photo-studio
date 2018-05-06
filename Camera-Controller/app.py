acc9763036a93d11567d070498f91dc7b65ec788
Eric Ratliff <e@edr.xyz>
2018-05-06 01:21:04 +0000

    Project now works with a raspberry pi

--- Camera-Controller/app.py
+++ Camera-Controller/app.py
@@ -1,11 +1,9 @@
 import logging
 import os
 import gphoto2 as gp
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 def take_photo():
 
--- Photo-Mod/app.py
+++ Photo-Mod/app.py
@@ -1,8 +1,6 @@
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
