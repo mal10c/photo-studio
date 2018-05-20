fa21bcabb1d0a6ecef367d2c36a76bf10aacb8de
Eric Ratliff <e@edr.xyz>
2018-05-20 11:46:54 -0500

    Photos can now be retrieved using an img tag within the photo album.

---
+++ Photo-Album/app.py
@@ -0,0 +1,33 @@
+import time
+import os
+from flask import Flask, request, redirect, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/photos/<path:path>')
+def send_photos(path):
+    return send_from_directory('/photos', path)
+
+@app.route('/<path:path>')
+def catch_all(path):
+
+    result = ""
+
+    if path == "album":
+        folder = request.args.get("folder")
+        photoPath = "/photos/" + folder
+        if os.path.isdir(photoPath):
+            for photo in os.listdir(photoPath):
+                if photo.endswith("jpg"):
+                    entirePath = "http://172.16.0.1:5011{}/{}".format(photoPath, photo)
+                    result += "<img src=\"{}\" />".format(entirePath)
+        else:
+            result = "Directory not found"
+    else:
+        return "Invalid"
+    
+    return result
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
+
